152534dcf0e98fdf096c4bbd2b702149487ec1dd
tarunjawla <tarunjawla2@gmail.com>
2024-08-27 21:43:14 +0530

    added md file

--- src/mytest.test.ts
+++ src/mytest.test.ts
@@ -4,7 +4,7 @@ describe('calculator',()=>{
     it('should call multiply when adding two numbers', ()=>{
         const multiplySpy = jest.spyOn(calculator, 'multiply');
         const res = calculator.multiply(2,3);
-        expect(multiplySpy).toBeCalledWith(2,3);
+        expect(multiplySpy).toHaveBeenCalledWith(2,3);
         expect(res).toBe(6);
 
         multiplySpy.mockRestore();
